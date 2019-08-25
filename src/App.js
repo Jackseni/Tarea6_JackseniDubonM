fb477432272654357ff0305f454620108805e697
Jackseni <50254824+Jackseni@users.noreply.github.com>
2019-08-24 22:11:21 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class Name extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {name: 'señor desconocido'}
+  }
+  changeName() {
+    let n = document.getElementsByClassName('input')[0].value
+    this.setState({name: n})
+  }
+  render() {
+    return (
+      <div className="App-sayname">
+        <input type="name" className="input" placeholder="Tu nombre" onChange={this.changeName.bind(this)}/>
+        <h2 className="input-helper">Bienvenido {this.state.name}!</h2>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome Jackseni</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <Name/>
+      </div>
+    );
+  }
 }
 
 export default App;
+
